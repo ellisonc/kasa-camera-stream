7c6f88d56a214f71a3dc883ab512ca3bdb9032f6
ellisonc <ellisonc@wustl.edu>
2021-08-29 19:47:58 -0500

    add mulitcam

--- Controller/Wrapper.py
+++ Controller/Wrapper.py
@@ -7,44 +7,46 @@ import HealthChecker
 class FfmpegWrapper:
     def startProcess(self):
         print("[Wrapper] Starting Ffmpeg.")
-        startCommand = [
-            'ffmpeg',
-            '-nostdin',
-            '-loglevel',
-            'warning',
-            '-reconnect',
-            '1',
-            '-reconnect_at_eof',
-            '1',
-            '-reconnect_streamed',
-            '1',
-            '-reconnect_delay_max',
-            '60',
-            '-headers',
-            f'Authorization: Basic {self.authToken}',
-            '-f',
-            'h264',
-            '-i',
-            f'https://{self.controller.config["cameraip"]}:19443/https/stream/mixed?video=h264&audio=g711&resolution=hd',
-            '-map',
-            '0',
-            '-vcodec',
-            'copy',
-            '-preset',
-            'veryfast',
-            '-f',
-            'flv',
-            f'rtmp://localhost/live/{self.controller.config["cameraname"]}',
-            '-map',
-            '0',
-            '-r',
-            '1/5',
-            '-update',
-            '1',
-            '-y',
-            f'/tmp/streaming/thumbnails/{self.controller.config["cameraname"]}.jpg'
-        ]
-        self.ffmpegProcess = subprocess.Popen(startCommand)
+        
+        for cam in self.controller.config["cams"]:
+            startCommand = [
+                'ffmpeg',
+                '-nostdin',
+                '-loglevel',
+                'warning',
+                '-reconnect',
+                '1',
+                '-reconnect_at_eof',
+                '1',
+                '-reconnect_streamed',
+                '1',
+                '-reconnect_delay_max',
+                '60',
+                '-headers',
+                f'Authorization: Basic {self.authToken}',
+                '-f',
+                'h264',
+                '-i',
+                f'https://{cam["cameraip"]}:19443/https/stream/mixed?video=h264&audio=g711&resolution=hd',
+                '-map',
+                '0',
+                '-vcodec',
+                'copy',
+                '-preset',
+                'veryfast',
+                '-f',
+                'flv',
+                f'rtmp://localhost/live/{cam["cameraname"]}',
+                '-map',
+                '0',
+                '-r',
+                '1/5',
+                '-update',
+                '1',
+                '-y',
+                f'/tmp/streaming/thumbnails/{cam["cameraname"]}.jpg'
+            ]
+            self.ffmpegProcess = subprocess.Popen(startCommand)
 
     def buildAuthToken(self):
         encodedPassword = self.encode(self.controller.config['kasapassword'])
